a6b559e3a6c02783efab329e3bb9972ec2310511
Daniel W. Robert <danielwrobert@gmail.com>
2018-12-14 10:44:24 -0500

    Swapped MySQL command for wp db create.

--- wp-init.sh
+++ wp-init.sh
@@ -3,11 +3,11 @@
 echo "Downloading the latest version of WordPress..."
 wp core download
 
-echo "Creating a new database for your project..."
-echo "CREATE DATABASE $1" | mysql -u root
-
 echo "Creating a WordPress config file..."
 wp config create --dbname=$1 --dbuser=root
+
+echo "Creating a new database for your project..."
+wp db create
 
 echo "Installing WordPress..."
 wp core install --url=$1.test --title="$2" --admin_user=$3 --admin_password=$4 --admin_email=$5
