18e7a2932f5d21cb108bc156e5c44314b2a45193
Daniel W. Robert <danielwrobert@gmail.com>
2019-01-09 22:11:54 -0500

    Added prompts and defaults for greater flexibility.

--- wp-init.sh
+++ wp-init.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+currentdir=${PWD##*/}
+
 echo "Downloading the latest version of WordPress..."
 wp core download
 
 echo "Creating a WordPress config file..."
-wp config create --dbname=$1 --dbuser=root
+read -p "Please enter the database name (default = ${currentdir}): " dbname
+dbname=${dbname:-${currentdir}}
+read -p "Please enter the database user (default = root): " dbuser
+dbuser=${dbuser:-root}
+wp config create --dbname=${dbname} --dbuser=${dbuser}
 
 echo "Creating a new database for your project..."
 wp db create
 
 echo "Installing WordPress..."
-wp core install --url=$1.test --title="$2" --admin_user=$3 --admin_password=$4 --admin_email=$5
+read -p "Please enter the local development URL (default = ${currentdir}.test: " devurl
+devurl=${devurl:-${currentdir}.test}
+read -p "Please enter the site title (default = ${currentdir}): " title
+title=${title:-${currentdir}}
+read -p "Please enter the admin username (default = admin): " username
+username=${username:-admin}
+read -p "Please enter the admin password (default = password): " userpass
+userpass=${userpass:-password}
+read -p "Please enter the admin email: " useremail
+wp core install --url=${devurl} --title="${title}" --admin_user=${username} --admin_password=${userpass} --admin_email=${useremail}
 
 echo "Project setup complete ... now get to work!"
