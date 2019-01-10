965135ccbca02393b0bd81a2ab6851108b9f4ea0
Daniel W. Robert <danielwrobert@gmail.com>
2019-01-09 22:38:06 -0500

    Added colors to message output. Updated README.

--- README.md
+++ README.md
@@ -4,12 +4,6 @@ A set of WP-CLI commands I frequently run to start up a new WP project.
 
 ## Usage
 
-Add this script to the directory you wish to install your WP project and run the following command:
+Add this script to the directory you wish to install your WP project, execute it (`sh wp-init.sh`), and follow the prompts!
 
-```
-sh wp-init.sh <PROJECT DIRECTORY NAME> <SITE TITLE> <USERNAME> <PASSWORD> <USER EMAIL>
-```
-
-Note that if you have spaces in any of the above arguments, you need to wrap that argument in single quotes. The arguments must be passed in the order listed above.
-
-For the time being, the directory name and the database name should be the same. This script assumes you can access your MySQL instance via `mysql -u root`. It works well in a Laravel Valet setup, for example. It also assumes you have [WP-CLI](https://developer.wordpress.org/cli/commands/) setup and configured.
+This script assumes you have [WP-CLI](https://developer.wordpress.org/cli/commands/) setup and configured.
--- wp-init.sh
+++ wp-init.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+YELLOW="\033[1;33m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
 currentdir=${PWD##*/}
 
-echo "Downloading the latest version of WordPress..."
+echo "${YELLOW}Grabbing the latest version of WordPress...${NOCOLOR}"
 wp core download
 
-echo "Creating a WordPress config file..."
+echo "${YELLOW}Creating a WordPress config file...${NOCOLOR}"
 read -p "Please enter the database name (default = ${currentdir}): " dbname
 dbname=${dbname:-${currentdir}}
 read -p "Please enter the database user (default = root): " dbuser
 dbuser=${dbuser:-root}
 wp config create --dbname=${dbname} --dbuser=${dbuser}
 
-echo "Creating a new database for your project..."
+echo "${YELLOW}Creating a new database for your project...${NOCOLOR}"
 wp db create
 
-echo "Installing WordPress..."
+echo "${YELLOW}Installing WordPress...${NOCOLOR}"
 read -p "Please enter the local development URL (default = ${currentdir}.test): " devurl
 devurl=${devurl:-${currentdir}.test}
 read -p "Please enter the site title (default = ${currentdir}): " title
@@ -27,4 +30,4 @@ userpass=${userpass:-password}
 read -p "Please enter the admin email: " useremail
 wp core install --url=${devurl} --title="${title}" --admin_user=${username} --admin_password=${userpass} --admin_email=${useremail}
 
-echo "Project setup complete ... now get to work!"
+echo "${GREEN}Project setup complete ... now get to work!${NOCOLOR}"
